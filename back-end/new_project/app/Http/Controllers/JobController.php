1363c1e2a1423024ee6a940a64affc8a8947cbd0
Mohamed Rashad <mohamed.a7med0101120@gmail.com>
2025-05-10 15:24:30 +0300

    update api - error handling

--- back-end/new_project/app/Http/Controllers/JobController.php
+++ back-end/new_project/app/Http/Controllers/JobController.php
@@ -3,40 +3,49 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\JobRequest;
+use App\Http\traits\ApiResponses;
 use App\Models\Category;
 use App\Models\Job;
 use Illuminate\Http\Request;
 
+
 class JobController extends Controller
 {
+    use ApiResponses;
     public function index(){
         $jobs = Job::all();
-        return response()->json(compact('jobs'));
+        return $this->data(compact('jobs'));
+        // return response()->json(compact('jobs'));
     }
     public function create(){
         $categories = Category::all();
-        return response()->json(compact('categories'));
+        return $this->data(compact('categories'));
+        // return response()->json(compact('categories'));
     }
 
     public function store(JobRequest $request){
         Job::create($request->all());
-        return response()->json(['status'=>true,'message'=>'Job created successfully']);
+        return $this->success('Job created successfully',201);
+        // return response()->json(['status'=>true,'message'=>'Job created successfully']);
     }
 
     public function edit($id){
         $job = Job::findOrFail($id);
         $categories = Category::all();
-        return response()->json(compact('job', 'categories')); 
+        return $this->data(compact('job', 'categories'));
+        // return response()->json(compact('job', 'categories')); 
     }
 
     public function update(JobRequest $request, $id){
         Job::where('id',$id)->update($request->all());
-        return response()->json(['status'=>true,'message'=>'Job updated successfully']);
+        return $this->success('Job updated successfully',200);
+        // return response()->json(['status'=>true,'message'=>'Job updated successfully']);
     }
 
     public function destroy($id){
         Job::where('id',$id)->delete();
-        return response()->json(['status'=>true,'message'=>'Job deleted successfully']);
+        return $this->success('Job deleted successfully',200);
+        // return response()->json(['status'=>true,'message'=>'Job deleted successfully']);
     }
     
     
